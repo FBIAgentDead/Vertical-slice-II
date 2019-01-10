a17241f146645b2541a915c8196e4944ea7ad4ab
NoahHoi365 <24922@ma-web.nl>
2019-01-10 12:14:21 +0100

    added statistics UI + follow

---
+++ Vertical Slice II/Assets/Scripts/UI/ShowStats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ShowStats : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    public Image statUI;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        statUI.enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        statUI.enabled = false;
+
+        //particles poof
+    }
+}
