5c54ab9f737deeae970a5a39800e03419b2d04d7
ItsStanMarsh <35196896+ItsStanMarsh@users.noreply.github.com>
2019-01-14 11:35:07 +0100

    credits and some random things

---
+++ Vertical Slice II/Assets/Scripts/SceneSwitch/TextMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextMove : MonoBehaviour {
+
+
+	void Move ()
+    {
+        transform.position += Vector3.up * Time.deltaTime;
+	}
+
+
+}
