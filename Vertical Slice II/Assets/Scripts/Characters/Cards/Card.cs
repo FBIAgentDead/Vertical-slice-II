e81648c4e68b573edaa906da4bed575987c2a364
DimitrivanManen <17382@ma-web.nl>
2019-01-18 15:11:00 +0100

    Fixed issue hover efect

---
+++ Vertical Slice II/Assets/Scripts/Characters/Cards/Card.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CardType {HealthBuf, DamageBuf, ManaBuf, Shield, LASTINT}
+public class Card{
+	
+    private float enumCount = (int)CardType.LASTINT;
+	public int GetHealthBuf {get; set;}
+	public int GetDamageBuf {get; set;}
+	public int GetManaBuf {get; set;}
+	public int GetPrice {get; set;}
+	public CardType GetCardType {get; set;}
+
+	public Card(){
+		// RandomizeCard();
+		Generate();
+	}
+
+	public virtual void Generate(){
+		
+	}
+
+	// private void RandomizeCard(){
+	// 	int i = Random.Range(0,Mathf.RoundToInt(enumCount));
+	// 	// Debug.Log(i);
+	// 	switch(i){
+	// 		case 0:
+	// 		GetCardType = CardType.HealthBuf;
+	// 		GetHealthBuf = GenerateCardValue();
+	// 		break;
+	// 		case 1:
+    //         GetCardType = CardType.DamageBuf;
+	// 		GetDamageBuf = GenerateCardValue();
+	// 		break;
+    //         case 2:
+    //         GetCardType = CardType.ManaBuf;
+	// 		GetManaBuf = GenerateCardValue();
+    //         break;
+	// 		case 3:
+	// 		//wat doen we hier?
+	// 		GetCardType = CardType.Shield;
+	// 		break;
+	// 	}
+	// }
+
+	// private int GenerateCardValue(){
+	// 	int buf = Random.Range(20, 100);
+	// 	GetPrice = buf/2;
+	// 	return buf;
+	// }
+
+}
