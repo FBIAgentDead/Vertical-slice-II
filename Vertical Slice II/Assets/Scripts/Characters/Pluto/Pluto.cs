335d92c8bfdf02a2f6e21e8802210fb3ea785605
DimitrivanManen <17382@ma-web.nl>
2019-02-01 21:27:39 +0100

    Added 3d models and animations into the main game

--- Vertical Slice II/Assets/Scripts/Characters/Pluto/Pluto.cs
+++ Vertical Slice II/Assets/Scripts/Characters/Pluto/Pluto.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class Pluto : MonoBehaviour, IAbilities {
     CharacterStats getStats;
+    Animator playAbilitie;
+    public GameObject enemyObject;
+    private Animator enemy;
+
+    void Awake()
+    {
+        playAbilitie = GetComponentInChildren<Animator>();
+        enemy = enemyObject.GetComponentInChildren<Animator>();
+    }
+
     public void SpecialAttack(){
         getStats = gameObject.GetComponent<CharacterStats>();
         if (getStats.canAttack){
             Debug.Log("SpecialAttack (Pluto)");
             getStats.mana -= 30;
+            playAbilitie.Play("AttackAnim");
+            enemy.Play("SelfHeal");
         }
 	}
     public void MainAttack(){
@@ -16,13 +28,16 @@ public class Pluto : MonoBehaviour, IAbilities {
         if(getStats.canAttack){
             Debug.Log("MainAttack (Pluto)");
             getStats.mana -= 70;
+            playAbilitie.Play("AttackAnim");
+            enemy.Play("SelfHeal");
         }
 	}
 
     public void SelfBuf(){
         getStats = gameObject.GetComponent<CharacterStats>();
         if(getStats.canAttack){
-        
+            playAbilitie.Play("Hit");
+            getStats.health += 10;
         }
     }
 
